cac3345fb03c14247c93adffb5d2eb19934d251c
rockdrigo <correo.rodrigo@gmail.com>
2022-11-03 09:18:09 -0500

    grpc service

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const grpc = require('@grpc/grpc-js');
+const greets  = require('./protos/greet_pb');
+const service = require('./protos/greet_grpc_pb');
+
+/*
+  Implements the greet RPC method.
+*/
+
+function greet(call, callback) {
+    let greeting = new greets.GreetResponse();
+    greeting.setResult(`
+        Hello ${call.request.getGreeting().getFirstname()}
+    `);
+
+    callback(null, greeting);
+}
 
 function main () {
     const server = new grpc.Server();
+    server.addService(service.GreetServiceService, {
+	greet: greet
+    });
 
     const address = "127.0.0.0:50051";
 
